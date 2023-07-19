ddb43e74819cbe66e4d3337aec4a9801f1c0c1d5
tabascum <marques_vasco@yahoo.es>
2023-07-19 20:08:01 +0200

    Update slider behavior

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,16 +5,42 @@ const monthlyPrice = [8, 12, 16, 24, 36];
 
 const views = document.querySelector("#views");
 const price = document.querySelector("#price");
+const yearlyPrice = document.querySelector("#yearly-price");
+const billingSwitch = document.querySelector("#billing");
+let yearly = false;
 
 slider.addEventListener("input", () => {
   let inputValue = slider.value;
-  let yearly = false;
 
-  if (yearly) {
-    price.innerHTML = monthlyPrice[inputValue] * 0.75 + `.00$ `;
-  } else {
-    price.innerHTML = monthlyPrice[inputValue] + `.00$`;
-  }
-
+  updateValue(yearly, inputValue);
   views.innerHTML = pageViews[inputValue] + ` Pageviews`;
+
+  let value = inputValue * 25;
+
+  slider.style.background = `linear-gradient(to right,
+            hsl(174, 77%, 80%) 0%,
+            hsl(174, 77%, 80%) ${value}%,
+            hsl(224, 65%, 95%) 0%,
+            hsl(224, 65%, 95%) 100%)`;
 });
+
+billingSwitch.addEventListener("change", () => {
+  if (yearly != true) {
+    yearly = true;
+    yearlyPrice.innerHTML = `$` + monthlyPrice[inputValue] * 0.75 + `.00 `;
+  } else {
+    yearly = false;
+    price.innerHTML = `\$${monthlyPrice[inputValue]}.00`;
+  }
+  updateValue();
+});
+
+function updateValue(yearly, inputValue) {
+  if (yearly) {
+    price.innerHTML = `$` + monthlyPrice[inputValue] * 0.75 + `.00 `;
+    yearlyPrice.innerHTML = `$` + monthlyPrice[inputValue] * 0.75 + `.00 `;
+  } else {
+    price.innerHTML = `\$${monthlyPrice[inputValue]}.00`;
+    yearlyPrice.innerHTML = `\$${monthlyPrice[inputValue]}.00`;
+  }
+}
