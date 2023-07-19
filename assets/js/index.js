a33d774a9514d17e5797f2de41a18eea65580616
tabascum <marques_vasco@yahoo.es>
2023-07-19 12:02:31 +0200

    Add mobile layout. Link slider to html elements which will be dynamic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,20 @@
 const slider = document.querySelector(".slider");
 
 const pageViews = ["10K", "50K", "100K", "500K", "1M"];
-const priceValueMonth = [8, 12, 16, 24, 36];
+const monthlyPrice = [8, 12, 16, 24, 36];
 
 const views = document.querySelector("#views");
 const price = document.querySelector("#price");
 
 slider.addEventListener("input", () => {
-  this.style.background = `linear-gradient(to right,
-        hsl(174, 77%, 80%) 0%,
-        hsl(174, 77%, 80%) ${this.value}%,
-        hsl(224, 65%, 95%) 0%,
-        hsl(224, 65%, 95%) 100%)`;
+  let inputValue = slider.value;
+  let yearly = false;
+
+  if (yearly) {
+    price.innerHTML = monthlyPrice[inputValue] * 0.75 + `.00$ `;
+  } else {
+    price.innerHTML = monthlyPrice[inputValue] + `.00$`;
+  }
+
+  views.innerHTML = pageViews[inputValue] + ` Pageviews`;
 });
